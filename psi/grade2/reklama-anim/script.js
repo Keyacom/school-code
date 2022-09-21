589f5fa3ec5b75c8be8e768dddb8f838dc7e72a5
Keyacom <70766223+Keyacom@users.noreply.github.com>
2022-09-21 22:07:29 +0200

    Added more files and an ESLint dep

--- psi/grade2/reklama-anim/script.js
+++ psi/grade2/reklama-anim/script.js
@@ -1,2 +1,7 @@
-const consoleWindow = document.querySelector("#main");
+const consoleWindow = document.querySelector("#con");
 
+const {
+  ioInput, ioOutput, ioIbefore, ioObefore
+} = consoleWindow.dataset;
+
+consoleWindow.innerText = ioIbefore;
