cb9d028874b1f993b628b01b8774810a412b954b
Keyacom <70766223+Keyacom@users.noreply.github.com>
2022-09-25 20:30:07 +0200

    Expanded project

--- pai/grade2/jquery/lib/field_class.js
+++ pai/grade2/jquery/lib/field_class.js
@@ -1,22 +1,29 @@
+/**
+ * Class used for creating a form field.
+ * */
 class Field {
-  /**
-   * Class used for creating a form field.
-   * */
   constructor(o) {
     this.minLen = o.minLen ?? 0;
     this.required = o.required ?? false;
     this.maxLen = o.maxLen ?? 0;
-    this.max = o.max ?? 999;
+    this.max = o.max ?? 0;
     this.min = o.min ?? 0;
-    this.type = o.type?.trim?.().toLowerCase() ?? '';
+    this.type = o.type?.trim?.().toLowerCase() ?? 'text';
     this.inputType = o.inputType?.trim?.().toLowerCase() ?? 'text';
     this.innerName = o.innerName ?? ''
     this.value = o.value ?? ''
   }
+  /**
+   * Render a field.
+   * 
+   * @param {*} d The field's visible name.
+   * @param {*} n The field's internal name.
+   * @returns {string} The rendered HTML.
+   */
   render(d, n = this.innerName) {
     return `<label for="${n}"><b>${d}</b></label>
     <div class="field">
-    <${this.inputType === "textarea" ? "textarea" : "input"}
+    <${this.inputType === "textarea" ? 'textarea rows="6"' : "input"}
       id="${n}"
       name="${n}"
       value="${this.value}"
--- pai/grade2/jquery/projekt/README.md
+++ pai/grade2/jquery/projekt/README.md
@@ -2,6 +2,6 @@
 
 A project site about Pokémon that I made in late September 2022.
 
-Credit to Pokémon artworks goes to [Ken Sugimori](https://twitter.com/SUPER_32X).
+Credit to Pokémon artworks goes to [Ken Sugimori](https://twitter.com/SUPER_32X). The Mew sprites come from the [Pokémon Showdown Pokédex](https://dex.pokemonshowdown.com/).
 
 After this project, we'll start learning about PHP.
